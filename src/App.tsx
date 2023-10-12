9270af39bda84420ea023116b16d555827bcc601
VladimirS <v.a.samarin@yandex.ru>
2023-10-12 21:03:01 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false}
 
+
     ]
     const tasks2 = [
         { id: 1, title: "Hello world", isDone: true },
